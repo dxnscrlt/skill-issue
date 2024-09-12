7f500271b6e6134a0c52666c3227863bea640290
K <dx.scarlet.xn@gmail.com>
2024-09-11 22:30:12 -0400

    Create addQQwfeedback.py
    Create a Python program designed to add two rational numbers written in decimalized form. Program receives feedback, however unable to repeat when feedback received is “N”.

---
+++ addQQwfeedback.py
@@ -0,0 +1,11 @@
+#This program is designed to add two rational numbers.
+print("Hello! Let's get started on adding two rational numbers.")
+a = float(input("Enter the first addend:")
+print("You entered:", a)
+b = float(input("Enter the second addend:")
+print("You entered:", b, "\nThe sum is:", a+b)
+c = input("Is this correct? (Y/N)")
+if c == "Y":
+  print("Well done!")
+elif c == "N":
+  print("Let's try that again!")
