e6cf9bc4ca998ed0ecf48733d4f4f04c9c10ced3
K <dx.scarlet.xn@gmail.com>
2024-10-16 17:44:17 -0400

    Update guessrandomint.py

--- guessrandomint.py
+++ guessrandomint.py
@@ -1,15 +1,27 @@
 #This program generates a guessing game, with optional conditions.
-#Guessing game by importing 'random' library
 import random
+
+#Setting the upper limit:
 lower = 0
 upper = int(input("Enter a positive integer:\n"))
 while upper <= 0:
-    if upper <= 0:
-        upper = int(input("Invalid input. Try again!\n"))
-        continue
-    else:
-        break
+    upper = int(input("Invalid input. Try again!\n"))
+    
+#Choosing handicap conditions:
+oddeven = str(input("Would you like to know if the number is odd or even? Y/N\n"))
+while oddeven.lower() != 'y' and oddeven.lower() != 'n':
+    oddeven = str(input("Invalid input. Try again!\n"))
+highlow = str(input("Would you like to know if your current guess is higher or lower than the number? Y/N\n"))
+while highlow.lower() != 'y' and highlow.lower() != 'n':
+    highlow = str(input("Invalid input. Try again!\n"))
+
+#Game
 rnum = random.randint(lower, upper)
+if oddeven.lower() == 'y':
+    if rnum % 2 == 0:
+        print(f'The number I\'m thinking is even.')
+    else:
+        print(f'The number I\'m thiniking is odd.')
 gnum = int(input(f"Guess what number I'm thinking from {lower} to {upper}...\n"))
 attempt = 0
 while attempt <= 3:
@@ -18,6 +30,11 @@ while attempt <= 3:
         continue
     else:
         if rnum != gnum:
+            if highlow.lower() == 'y':
+                if gnum > rnum:
+                    print(f'{gnum} is too high.')
+                elif gnum < rnum:
+                    print(f'{gnum} is too low.')
             gnum = int(input(f'You guessed wrong! You have {3 - attempt} attempts left.\n'))
             attempt += 1
             if attempt == 3:
