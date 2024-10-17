520c743735a1c1009f2dc70ab9fd1ac77cabe7b6
K <dx.scarlet.xn@gmail.com>
2024-10-16 20:03:53 -0400

    Update addfloatwfeedback.py

--- addfloatwfeedback.py
+++ addfloatwfeedback.py
@@ -1,12 +1,18 @@
 #This program is designed to add two rational numbers.
 print("Hello! Let's get started on adding two rational numbers.")
-a = float(input("Enter the first addend:"))
-print("You entered:", a)
-b = float(input("Enter the second addend:"))
-print("You entered:", b, "\nThe sum is:", a+b)
-c = input("Is this correct? (Y/N)")
-if c == "Y":
-    print("Well done!")
-elif c == "N":
-    print("Let's try that again!")
-#Pending Issue: no program response if c =/= Y or N
+c = ''
+while c.upper() != 'Y':
+    a = float(input("Enter the first addend:"))
+    print("You entered:", a)
+    b = float(input("Enter the second addend:"))
+    print("You entered:", b, "\nThe sum is:", a+b)
+    c = input("Is this correct? (Y/N)")
+    if c.upper() == "Y":
+        print("Well done!")
+        break
+    elif c.upper() == "N":
+        print("Let's try that again!")
+        continue
+    else:
+        print("Invalid input.\nProgram ended.")
+#Pending Issue: negative float addends generate incorrect sum estimations.
